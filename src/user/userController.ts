f99de0db2b06c94ecb57e58edfad2040ca9a3f0d
Aman Ahamed <amanahamed04gmail.com>
2024-05-12 18:15:10 +0530

    password hash

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { Request, NextFunction, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -16,6 +17,8 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "User Already exists with this email");
     return next(error);
   }
+  // password hash
+  const hashedPassword = await bcrypt.hash(password, 10);
   // process
   // response
   res.json({ message: "User Created" });
