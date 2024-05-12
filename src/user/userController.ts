b2d559a2cd2c2728436a95fcb7acb73921c61595
Aman Ahamed <amanahamed04gmail.com>
2024-05-12 17:17:34 +0530

    register user validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,15 @@
 import { Request, NextFunction, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+  // Validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+  // process
+  // response
   res.json({ message: "User Created" });
 };
 
