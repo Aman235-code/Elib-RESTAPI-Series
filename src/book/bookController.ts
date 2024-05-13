918abf68c6e34b591b414fda76e894bdae7b33ea
Aman Ahamed <amanahamed04gmail.com>
2024-05-13 21:08:01 +0530

    create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,7 +2,10 @@ import { Request, NextFunction, Response } from "express";
 import path from "path";
 import cloudinary from "../config/cloudinary";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import fs from "fs";
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, genre } = req.body;
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
   const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
   const filename = files.coverImage[0].filename;
@@ -37,7 +40,20 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     );
     console.log(bookFileUploadResult);
     console.log(uploadResult);
-    res.json({});
+
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "6640d037934dead720140446",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    // Delete temp files
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookFilePath);
+
+    res.status(201).json({ id: newBook._id });
   } catch (error) {
     return next(createHttpError(500, "Error While Uploading the files"));
   }
